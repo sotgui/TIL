afceb6a3c35f3350f6821434d76df9c28e516cd8
sotgui <gus3779@gmail.com>
2022-01-24 23:18:06 +0900

    subcommit

---
+++ boj/Sort/boj11651.java
@@ -0,0 +1,18 @@
+package boj;
+import java.util.*;
+public class boj11651 {
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int[][] xy = new int[][2];
+	for(int i = 0; i<n = i++) {
+		xy[i][0] = sc.nextInt();
+		xy[i][1] = sc.nextInt();
+	}
+	Comparator<int[]> comp = new Comparator<int[]>() {
+		public int compare(int[] a1, int[] a2) {
+			if(a1[0][0] > a2[0][1]) {
+				
+			}
+		}
+	}
+}
