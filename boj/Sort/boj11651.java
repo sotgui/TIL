19b7f954a13d56525a0487ddb833ccb407817b1f
sotgui <gus3779@gmail.com>
2022-01-25 23:37:06 +0900

    alchoholic code (Comp2)

--- boj/Sort/boj11651.java
+++ boj/Sort/boj11651.java
@@ -1,18 +1,27 @@
 package boj;
 import java.util.*;
 public class boj11651 {
-	Scanner sc = new Scanner(System.in);
-	int n = sc.nextInt();
-	int[][] xy = new int[][2];
-	for(int i = 0; i<n = i++) {
-		xy[i][0] = sc.nextInt();
-		xy[i][1] = sc.nextInt();
-	}
-	Comparator<int[]> comp = new Comparator<int[]>() {
-		public int compare(int[] a1, int[] a2) {
-			if(a1[0][0] > a2[0][1]) {
-				
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] xy = new int[n][2];
+		for(int i = 0; i<n ; i++) {
+			xy[i][0] = sc.nextInt();
+			xy[i][1] = sc.nextInt();
+		}
+		Comparator<int[]> comp = new Comparator<int[]>() {
+			public int compare(int[] a1, int[] a2) {
+				if(a1[1] == a2[1]) {
+					return a1[0] - a2[0];
+				}else {
+					return a1[1] - a2[1];
+				}
 			}
+		};
+		Arrays.sort(xy, comp);
+		
+		for(int i = 0; i<n; i++) {
+			System.out.println(xy[i][0] + " " + xy[i][1]);
 		}
 	}
 }
