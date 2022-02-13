e10f9a5f29f643b2271e106f866de10d32780f4b
sotgui <gus3779@gmail.com>
2022-02-13 23:31:46 +0900

    Dynamic Programming basic

---
+++ boj/동적계획법1/boj9184.java
@@ -0,0 +1,39 @@
+package boj;
+import java.util.*;
+public class boj9184 {
+	static int[][][] ans = new int[21][21][21];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		for(int[][] arr : ans) {
+			for(int[] ar : arr) {
+				Arrays.fill(ar, -1);
+			}
+		}
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			if(a==-1 && b==-1 && c==-1) break;
+			System.out.println("w("+a+", "+b+", "+c+") = " + w(a,b,c));
+		}
+	}
+	static int w(int a, int b, int c) {
+		if(a <= 0 || b <= 0 || c <= 0) {
+			return 1;
+		}
+		if(a > 20 || b > 20 || c > 20) {
+			if(ans[20][20][20] == -1) {
+				ans[20][20][20] = w(20, 20, 20);
+			}
+			return ans[20][20][20];
+		}
+		if(ans[a][b][c] == -1) {
+			if(a < b && b < c) {
+				ans[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+			}else {
+				ans[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+			}
+		}
+		return ans[a][b][c];		
+	}
+}
