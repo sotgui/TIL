7793379f5552a31157774335f6042cda135dd10c
sotgui <gus3779@gmail.com>
2022-02-23 23:29:02 +0900

    눈 똑바로 뜨고 풀자..

--- boj/동적계획법1/boj10844.java
+++ boj/동적계획법1/boj10844.java
@@ -2,34 +2,34 @@ package boj;
 import java.util.*;
 public class boj10844 {
 	static int n;
-	static int[][] ans;
+	static long[][] ans;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		ans = new int[n+1][10];
-		for(int[] arr : ans) {
+		ans = new long[n+1][10];
+		for(long[] arr : ans) {
 			Arrays.fill(arr, -1);
 		}
-		int answer = 0;
+		long answer = 0;
 		for(int i = 1; i<10; i++) {
 			answer += stairNum(n, i);
 		}
+		answer %= 1000000000;
 		System.out.println(answer);
 	}
-	static int stairNum(int num, int i) {
+	static long stairNum(int num, int i) {
 		if(num == 1) {
-			return 1;
+			ans[num][i] = 1;
 		}
 		if(ans[num][i] == -1) {
 			if(i == 0) {
-				ans[num][i] = stairNum(n-1, 1) % 1000000000;
+				ans[num][i] = stairNum(num-1, 1);
 			}else if(i == 9) {
-				ans[num][i] = stairNum(n-1, 8) % 1000000000;
+				ans[num][i] = stairNum(num-1, 8);
 			}else {
-				ans[num][i] = stairNum(n-1, i-1) + stairNum(n-1, i+1);
-				ans[num][i] %= 1000000000;
+				ans[num][i] = stairNum(num-1, i-1) + stairNum(num-1, i+1);
 			}
 		}
-		return ans[num][i];
+		return ans[num][i] % 1000000000;
 	}
 }
