64dbaaa34aa5ad3658ec0bea1853283f571de311
sotgui <gus3779@gmail.com>
2022-03-01 14:27:13 +0900

    엥? 갑자기 쉽게 풀려버리기

--- boj/동적계획법1/boj2156.java
+++ boj/동적계획법1/boj2156.java
@@ -7,27 +7,28 @@ public class boj2156 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		wine = new int[n];
-		score = new int[n];
-		for(int i = 0; i<n; i++) {
+		wine = new int[n+1];
+		score = new int[n+1];
+		for(int i = 1; i<n+1; i++) {
 			wine[i] = sc.nextInt();
 		}
 		Arrays.fill(score, -1);
-		System.out.println(getScore(n-1));
+		System.out.println(getScore(n));
 	}
 	static int getScore(int i) {
 		if(i == 0) {
-			score[i] = wine[i];
+			score[i] = 0;
 		}
 		if(i == 1) {
-			score[i] = wine[i] + wine[i-1];
+			score[i] = wine[i];
 		}
 		if(i == 2) {
-			score[i] = wine[i] + Math.max(wine[i-2], wine[i-1]);
+			score[i] = wine[i] + wine[i-1];
 		}
 		if(score[i] == -1) {
-			score[i] = Math.max(wine[i] + Math.max(getScore(i-3), Math.max(getScore(i-2), getScore(i-3) + wine[i-1])), getScore(i-1));
+			score[i] = Math.max( wine[i] + Math.max(getScore(i-2), getScore(i-3) + wine[i-1]) , getScore(i-1) );
 		}
 		return score[i];
 	}
+	
 }
