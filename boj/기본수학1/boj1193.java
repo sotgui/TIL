2aa11aead670fbb0f57f030d4be4cf8870a96c1f
sotgui <gus3779@gmail.com>
2022-03-09 23:59:31 +0900

    2일 경우

--- boj/기본수학1/boj1193.java
+++ boj/기본수학1/boj1193.java
@@ -4,17 +4,23 @@ public class boj1193 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int x = 1;
-		int y = 1;
+		int count = n;
+		int a = 1;
+		int b = 1;
 		for(int i = 1; i<n; i++) {
-			if(x == 1) {
-				x = y + 1;
-				y = 1;
-			}else{
-				x--;
-				y++;
+			if(count - i <= 0) {
+				if(i % 2 == 0) {
+					a = count;
+					b = i - count + 1;
+				}else {
+					a = i - count + 1;
+					b = count;
+				}
+				break;
+			}else {
+				count -= i;
 			}
 		}
-		System.out.println(y + "/" + x);
+		System.out.println(a + "/" + b);
 	}
 }
