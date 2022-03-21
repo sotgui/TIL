64f0c3cd86e22f1ca95e2a29d5c803f18396b5ba
sotgui <gus3779@gmail.com>
2022-03-21 23:56:56 +0900

    내일 수정

--- boj/동적계획법1/boj9251.java
+++ boj/동적계획법1/boj9251.java
@@ -12,5 +12,21 @@ public class boj9251 {
 		B = str.toCharArray();
 		lcs = new int[A.length];
 		Arrays.fill(lcs, -1);
+		for(int i = 0; i<A.length; i++) {
+			dp(i);
+		}
+		System.out.println(lcs[A.length - 1] + 1);
+	}
+
+	static int dp(int k) {
+		if(lcs[k] == -1) {
+			lcs[k] = 1;		// 기본으로 길이 1이므로 항상 1로 초기화
+			for(int i = k-1; i>=0; i--) {
+				if(A[i] == B[k]) {	// 
+					lcs[k] = Math.max(lcs[k], dp(i) + 1);
+				}
+			}
+		}
+		return lcs[k];
 	}
 }
