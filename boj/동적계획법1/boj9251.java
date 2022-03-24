38cd105aac0cc822dca85159a00a9b5986296f57
sotgui <gus3779@gmail.com>
2022-03-25 00:44:41 +0900

    전혀 새로운 접근 필요..

--- boj/동적계획법1/boj9251.java
+++ boj/동적계획법1/boj9251.java
@@ -4,18 +4,37 @@ public class boj9251 {
 	static char[] A;
 	static char[] B;
 	static int[] lcs;
+	static int l;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-		A = str.toCharArray();
-		str = sc.next();
-		B = str.toCharArray();
-		lcs = new int[A.length];
+		String str1 = sc.next();
+		String str2 = sc.next();
+		int len1 = str1.length();
+		int len2 = str2.length();
+		if(len1 > len2) {
+			l = len1;
+			A = str1.toCharArray();
+			B = new char[l];
+			Arrays.fill(B, ' ');
+			for(int i = 0 ; i<len2; i++) {
+				B[i] = str2.charAt(i);
+			}
+		}else {
+			l = len2;
+			A = str2.toCharArray();
+			B = new char[l];
+			Arrays.fill(B, '-');
+			for(int i = 0 ; i<len1; i++) {
+				B[i] = str1.charAt(i);
+			}
+		}
+		lcs = new int[l];
 		Arrays.fill(lcs, -1);
-		for(int i = 0; i<A.length; i++) {
+		for(int i = l-1; i>=0; i--) {
 			dp(i);
 		}
-		System.out.println(lcs[A.length - 1]);
+		Arrays.sort(lcs);
+		System.out.println(lcs[l - 1]);
 	}
 
 	static int dp(int k) {
@@ -29,6 +48,7 @@ public class boj9251 {
 		if(lcs[k] == -1) {
 			lcs[k] = 1;		// 기본으로 길이 1이므로 항상 1로 초기화
 			for(int i = k; i>=0; i--) {
+				if(A[i] == '-' || B[i] == '-') continue;
 				if(A[i] == B[k]) {	// 
 					lcs[k] = Math.max(lcs[k], dp(k-1) + 1);
 				}
