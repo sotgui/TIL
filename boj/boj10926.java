e1187da6cf1891a4a834b2e4c6c1a56f1fe54e08
sotgui <gus3779@gmail.com>
2022-03-28 23:41:01 +0900

    백준 채점이 안돼??!

---
+++ boj/boj10926.java
@@ -0,0 +1,9 @@
+package boj;
+import java.util.*;
+public class boj10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(str + "??!");
+	}
+}
