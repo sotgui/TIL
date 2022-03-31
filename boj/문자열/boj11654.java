b2199b7670dd66f7af5b9a6cb5e506229d106405
sotgui <gus3779@gmail.com>
2022-03-31 23:58:06 +0900

    char to ASCII num

---
+++ boj/문자열/boj11654.java
@@ -0,0 +1,10 @@
+package boj;
+import java.util.*;
+public class boj11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		int n = (int)a.charAt(0);
+		System.out.println(n);
+	}
+}
