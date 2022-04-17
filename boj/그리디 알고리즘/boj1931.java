4db71a2169b19e49a1fa4468dbd576cde68bf355
sotgui <gus3779@gmail.com>
2022-04-17 22:48:04 +0900

    I\v

---
+++ boj/그리디 알고리즘/boj1931.java
@@ -0,0 +1,9 @@
+package boj;
+import java.util.*;
+public class boj1931 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		// 어 뭐야 나 이거 코테에서 비슷한거 쳤는데...!!
+	}
+}
