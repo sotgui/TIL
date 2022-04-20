d34c6275a287b10dd6c6dec8d274935861fa7900
sotgui <gus3779@gmail.com>
2022-04-20 23:55:27 +0900

    방심컷

---
+++ boj/문자열/boj1152.java
@@ -0,0 +1,10 @@
+package boj;
+import java.util.*;
+public class boj1152 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String[] result = str.split(" ");
+		System.out.println(result.length);
+	}
+}
