10035f9d669755ac451bd54d12114a9ba7d2f714
sotgui <gus3779@gmail.com>
2022-04-22 23:42:07 +0900

    StringTokenizer

--- boj/문자열/boj1152.java
+++ boj/문자열/boj1152.java
@@ -4,20 +4,7 @@ public class boj1152 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
-		String[] result = str.split(" ");
-		int count = 0;
-		for(int i = 0; i<result.length; i++) {
-			boolean flag = true;
-			for(int j = 0; j<result.length; j++) {
-				if(i == j) continue;
-				if(result[i].equals(result[j])) {
-					flag = false;
-				}
-			}
-			if(flag) {
-				count++;
-			}
-		}
-		System.out.println(count);
+		StringTokenizer st = new StringTokenizer(str," ");
+		System.out.println(st.countTokens());
 	}
 }
