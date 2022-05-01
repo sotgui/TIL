9d3e0c484c85670fba9be2372a65dc8fc65f873c
sotgui <gus3779@gmail.com>
2022-05-01 23:22:17 +0900

    예제 복붙했을 때 맨앞 스페이스 하나 사라짐

--- boj/boj25083.java
+++ boj/boj25083.java
@@ -1,6 +1,11 @@
 package boj;
 public class boj25083 {
 	public static void main(String[] args) {
-		System.out.print("        ,r'\"7\nr`-_   ,'  ,/\n\\. \". L_r'\n  `~\\/\n     |\n     |");
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
 	}
 }
